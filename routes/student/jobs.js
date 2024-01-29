8665407cf98b27c073c4e3d39006c9d27f9cb824
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2024-01-29 19:10:15 +0530

    CV Functionality Done

--- routes/student/jobs.js
+++ routes/student/jobs.js
@@ -69,7 +69,7 @@ router.put('/:jobId', async (req, res) => {
         // const studentIdToSearch = new ObjectId(req.user._id);
         // const student= await Student.findById(studentIdToSearch)
         const student=await prisma.student.findUnique({where:{id:req.body.studentId}})
-        if(!student.course||!student.department||!student.year||!student.cgpa||!student.resumeLink){
+        if(!student.course||!student.department||!student.year||!student.cgpa||!student.resumeId){
             res.status(401).json({
                 status:401,
                 message:"please complete your profile"
